42f2491b68f22cadcffab0e5fb7bab203840e25d
staffologic-lang <staffologic@gmail.com>
2026-05-29 11:43:31 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
